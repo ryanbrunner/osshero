c58faf3350648bb9cc0f25e76d6085f9440d281b
Ryan Brunner <ryan@ryanbrunner.com>
2011-05-18 12:13:53 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.beta1'
+gem 'mongoid'
+
+# Asset template engines
+gem 'haml'
+gem 'sass'
+gem 'compass'
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+  gem 'rspec-rails'
+  gem 'cucumber'
+  gem 'remarkable_activemodel'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osshero::Application.config.secret_token = '96d34e9a71907dafbad6f351d307b18dbca0f79409df1ef4c6a5276eaddb8e5a16839e30cf2303d03acd2ef107fc65fc9982705a5a75794c5c690561f31db8d8'
